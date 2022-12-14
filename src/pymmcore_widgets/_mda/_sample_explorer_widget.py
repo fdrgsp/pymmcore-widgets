b71b3aee414432838b51886fb419d974c9a978c3
fdrgsp <federico.gasparoli@gmail.com>
2022-12-13 20:56:47 -0500

    fix: comment

--- src/pymmcore_widgets/_mda/_sample_explorer_widget.py
+++ src/pymmcore_widgets/_mda/_sample_explorer_widget.py
@@ -140,7 +140,7 @@ class SampleExplorerWidget(MDAWidget):
 
         self.channel_groupbox.setMinimumHeight(175)
 
-        # groupbox for mda option QCollapsible
+        # groupbox for mda option QCollapsible.
         # move Time, Z Stack and Positions in a collapsible
         wdg = QGroupBox(title="MDA Options")
         wdg.setLayout(QVBoxLayout())
