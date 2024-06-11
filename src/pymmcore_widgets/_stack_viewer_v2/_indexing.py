0691a5fe9689b103646ed642e1992462b7848cc1
fdrgsp <federico.gasparoli@gmail.com>
2024-06-10 22:12:08 -0400

    fix: MM5DWriter sizes

--- src/pymmcore_widgets/_stack_viewer_v2/_indexing.py
+++ src/pymmcore_widgets/_stack_viewer_v2/_indexing.py
@@ -169,26 +169,10 @@ class MM5DWriter(DataWrapper["_5DWriterBase"]):
         return False
 
     def sizes(self) -> Mapping[Hashable, int]:
-        _sizes: dict[Hashable, int] = {}
-        if (sz := getattr(self._data, "sizes", None)) and isinstance(sz, Mapping):
-            sizes = sz
-            _sizes = {k: int(v) for k, v in sizes.items()}
-
-        elif (shp := getattr(self._data, "shape", None)) and isinstance(shp, tuple):
-            sizes = shp
-            for i, val in enumerate(sizes):
-                if isinstance(val, int):
-                    _sizes[i] = val
-                elif isinstance(val, Sequence) and len(val) == 2:
-                    _sizes[val[0]] = int(val[1])
-                else:
-                    raise ValueError(
-                        f"Invalid size: {val}. Must be an int or a 2-tuple."
-                    )
-        else:
-            _sizes = {}
-
-        return _sizes
+        try:
+            return super().sizes()
+        except NotImplementedError:
+            return {}
 
     def save_as_zarr(self, save_loc: str | Path) -> None:
         import zarr
