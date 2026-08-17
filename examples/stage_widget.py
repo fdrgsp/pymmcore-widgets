c5d3fcf6a902b6a4c0ccd5a502f3216a3260441d
fdrgsp <federico.gasparoli@gmail.com>
2026-08-17 11:46:35 -0400

    fix example

--- examples/stage_widget.py
+++ examples/stage_widget.py
@@ -5,7 +5,12 @@ in micromanager are displayed with a 'StageWidget'.
 """
 
 from pymmcore_plus import CMMCorePlus, DeviceType
-from qtpy.QtWidgets import QApplication
+from qtpy.QtWidgets import (
+    QApplication,
+    QGroupBox,
+    QHBoxLayout,
+    QWidget,
+)
 
 from pymmcore_widgets import StageWidget
 
@@ -14,27 +19,18 @@ app = QApplication([])
 mmc = CMMCorePlus().instance()
 mmc.loadSystemConfiguration()
 
-# wdg = QWidget()
-# wdg_layout = QHBoxLayout(wdg)
-
-# stages = list(mmc.getLoadedDevicesOfType(DeviceType.XYStage))
-# stages.extend(mmc.getLoadedDevicesOfType(DeviceType.Stage))
-# for stage in stages:
-#     lbl = "Z" if mmc.getDeviceType(stage) == DeviceType.Stage else "XY"
-#     bx = QGroupBox(f"{lbl} Control")
-#     bx_layout = QHBoxLayout(bx)
-#     bx_layout.setContentsMargins(0, 0, 0, 0)
-#     bx_layout.addWidget(StageWidget(device=stage, position_label_below=True))
-#     wdg_layout.addWidget(bx)
-
-
-# wdg.show()
-# app.exec()
+wdg = QWidget()
+wdg_layout = QHBoxLayout(wdg)
 
 stages = list(mmc.getLoadedDevicesOfType(DeviceType.XYStage))
 stages.extend(mmc.getLoadedDevicesOfType(DeviceType.Stage))
-xy = StageWidget(device=stages[0])
-xy.show()
-z = StageWidget(device=stages[1])
-z.show()
+for stage in stages:
+    lbl = "Z" if mmc.getDeviceType(stage) == DeviceType.Stage else "XY"
+    bx = QGroupBox(f"{lbl} Control")
+    bx_layout = QHBoxLayout(bx)
+    bx_layout.setContentsMargins(0, 0, 0, 0)
+    bx_layout.addWidget(StageWidget(device=stage))
+    wdg_layout.addWidget(bx)
+
+wdg.show()
 app.exec()
